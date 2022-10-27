2115f1c44aa38e293422a1b01d3056334f898843
Alino4ka0599 <115590007+Alino4ka0599@users.noreply.github.com>
2022-10-28 01:35:11 +0300

    Create task2.py

---
+++ labour4/task2.py
@@ -0,0 +1,18 @@
+def get_countchar(str):
+    str = str.lower()
+
+    dictionary = {}
+
+    for symbol in str:
+        if symbol.isalpha():
+            dictionary.update({symbol: str.count(symbol)})
+
+    return dictionary
+
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
